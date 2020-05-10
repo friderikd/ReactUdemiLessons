9029c703d778f6913a012abb723db07953486a86
Friderik Djubin <friderik@Frideriks-MacBook-Pro.local>
2020-05-10 23:35:28 +0300

    Character page almost done

---
+++ got-database/src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+<div class="loadingio-spinner-spinner-cdv50kx5rsn"><div class="ldio-d2h2fgo3pv">
+<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+</div></div>
+    )
+}
+
+export default Spinner;
