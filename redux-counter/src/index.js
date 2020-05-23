35be33298c7532c0661ac3c5cc9739de6bb335ae
Friderik Djubin <friderik@Frideriks-MacBook-Pro.local>
2020-05-24 01:24:58 +0300

    React added app rebuild to React+Redux all done

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -1,43 +1,16 @@
+import React from 'react';
+import ReactDom from 'react-dom';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import './style.css';
+import App from './components/app';
 
-// reducer
-const reducer = (state = 0, action) => {
-    switch (action.type) {
-        case 'INC':
-            return state +1;
-        case 'DEC':
-            return state -1;
-        case 'RES':
-            return 0
-        default:
-            return state
-    }
-}
+const store = createStore(reducer);
 
-// store init
-let store = createStore(reducer);
-
-// helping functions
-
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-const res = () => ({type: 'RES'});
-
-//buttons click check
-document.getElementById('inc').addEventListener('click', () => {
-    store.dispatch(inc())
-});
-document.getElementById('dec').addEventListener('click', () => {
-    store.dispatch(dec())
-});
-document.getElementById('res').addEventListener('click', () => {
-    store.dispatch(res())
-});
-
-// refresh counter function
-const update = () => {
-    document.getElementById('counter').innerText = store.getState();
-}
-
-//subscribe to store on every state update run update script
-store.subscribe(update);
+ReactDom.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    , document.getElementById('root')
+);
